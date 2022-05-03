13e14c9380bbc898eb2ce6dcf207f5a83cd0cc23
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-05-03 12:39:25 +0100

    mainLayOutScreen completed

--- screens/Dashboard/MainLayout.js
+++ screens/Dashboard/MainLayout.js
@@ -1,11 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, Image, Animated } from "react-native";
 import { Home, Profile, Search } from "../../screens";
 import { COLORS, SIZES, FONTS, constants } from "../../constants";
+import { Shadow } from "react-native-shadow-2";
+
+const bottom_tabs = constants.bottom_tabs.map((bottom_tabs) => ({
+  ...bottom_tabs,
+  ref: React.createRef(),
+}));
+
+const TabIndicator = ({ measureLayout, scrollX }) => {
+  const inputRange = bottom_tabs.map((_, i) => i * SIZES.width);
+  const tabIndicatorwidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measureLayout.map((measure) => measure.width),
+  });
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measureLayout.map((measure) => measure.x),
+  });
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        left: 0,
+        height: "100%",
+        width: tabIndicatorwidth,
+        borderRadius: SIZES.radius,
+        backgroundColor: COLORS.primary,
+        transform: [
+          {
+            translateX,
+          },
+        ],
+      }}
+    />
+  );
+};
+
+const Tabs = ({ scrollX, onBottomTabPress }) => {
+  const containerRef = React.useRef();
+  const [measureLayout, setMeasureLayout] = useState([]);
+  useEffect(() => {
+    let nl = [];
+
+    bottom_tabs.forEach((bottom_tab) => {
+      bottom_tab?.ref?.current?.measureLayout(
+        containerRef.current,
+        (x, y, width, height) => {
+          nl.push({
+            x,
+            y,
+            width,
+            height,
+          });
+          if (nl.length === bottom_tabs.length) {
+            setMeasureLayout(nl);
+          }
+        }
+      );
+    });
+  }, [containerRef.current]);
+
+  return (
+    <View
+      ref={containerRef}
+      style={{
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      {/* Tab Indicator */}
+      {measureLayout.length > 0 && (
+        <TabIndicator measureLayout={measureLayout} scrollX={scrollX} />
+      )}
+      {/* Tabs */}
+      {bottom_tabs.map((item, index) => {
+        return (
+          <TouchableOpacity
+            key={`BottomTab-${index}`}
+            ref={item.ref}
+            style={{
+              flex: 1,
+              paddingHorizontal: 15,
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            onPress={() => onBottomTabPress(index)}
+          >
+            <Image
+              source={item.icon}
+              reseizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+              }}
+            />
+            <Text
+              style={{
+                marginTop: 3,
+                color: COLORS.white,
+                fontSize: 16,
+              }}
+            >
+              {item.label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
 
 const MainLayout = () => {
   const flatListRef = React.useRef();
   const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  const onBottomTabPress = React.useCallback((bottomTabIndex) => {
+    flatListRef?.current.scrollToOffset({
+      offset: bottomTabIndex * SIZES.width,
+    });
+  });
 
   function renderContent() {
     return (
@@ -13,6 +128,7 @@ const MainLayout = () => {
         <Animated.FlatList
           ref={flatListRef}
           horizontal
+          scrollEnabled={false}
           pagingEnabled
           snapToAlignment="center"
           snapToInterval={SIZES.width}
@@ -42,6 +158,31 @@ const MainLayout = () => {
       </View>
     );
   }
+
+  function renderBottomTab() {
+    return (
+      <View
+        style={{
+          marginBottom: 20,
+          paddingHorizontal: SIZES.padding,
+          paddingVertical: SIZES.radius,
+        }}
+      >
+        <Shadow size={[SIZES.width - SIZES.padding * 2, 85]}>
+          <View
+            style={{
+              flex: 1,
+              borderRadius: SIZES.radius,
+              backgroundColor: COLORS.primary3,
+            }}
+          >
+            <Tabs scrollX={scrollX} onBottomTabPress={onBottomTabPress} />
+          </View>
+        </Shadow>
+      </View>
+    );
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: COLORS.white }}>
       <Text>Mainlayout</Text>
@@ -49,6 +190,7 @@ const MainLayout = () => {
       {renderContent()}
 
       {/* {BottomTab} */}
+      {renderBottomTab()}
     </View>
   );
 };
