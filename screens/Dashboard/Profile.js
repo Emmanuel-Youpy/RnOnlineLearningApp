f1e67ed7489d0dd7cdf183e794ec2ccea3e6fa83
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-05-10 17:47:36 +0100

    Profile Screen

--- screens/Dashboard/Profile.js
+++ screens/Dashboard/Profile.js
@@ -1,10 +1,119 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import { COLORS, SIZES, FONTS, icons, images } from "../../constants";
+import { IconButton, TextButton, LineDivider } from "../../components";
 
 const Profile = () => {
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 50,
+          paddingHorizontal: SIZES.padding,
+          justifyContent: "space-between",
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 27,
+            fontWeight: "bold",
+          }}
+        >
+          Profile
+        </Text>
+        <IconButton
+          icon={icons.sun}
+          iconStyle={{
+            tintColor: COLORS.black,
+          }}
+        />
+      </View>
+    );
+  }
+
+  function renderProfileCard() {
+    return (
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: SIZES.padding,
+          paddingHorizontal: SIZES.radius,
+          paddingVertical: 20,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.primary3,
+        }}
+      >
+        {/* Profile Image */}
+        <TouchableOpacity
+          style={{
+            width: 80,
+            height: 80,
+          }}
+        >
+          <Image
+            source={images.profile}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: 40,
+              borderWidth: 1,
+              borderColor: COLORS.white,
+            }}
+          />
+          <View
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              alignItems: "center",
+              justifyContent: "flex-end",
+            }}
+          >
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                marginBottom: -15,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 15,
+                backgroundColor: COLORS.primary,
+              }}
+            >
+              <Image
+                source={icons.camera}
+                reseizeMode="contain"
+                style={{
+                  width: 17,
+                  height: 17,
+                }}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>Profile</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
+      {/* Header */}
+      {renderHeader()}
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingHorizontal: SIZES.padding,
+          paddingBottom: 150,
+        }}
+      >
+        {/* Profile Card */}
+        {renderProfileCard()}
+      </ScrollView>
     </View>
   );
 };
