ef7981362a7ce8281c50700340e04fec18d221ea
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-05-16 03:09:48 +0100

    Scroll Animation

--- screens/Course/CourseListing.js
+++ screens/Course/CourseListing.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, BackHandler } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  BackHandler,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import Animated, {
   Extrapolate,
   interpolate,
@@ -15,28 +23,103 @@ import {
   HorizontalCourseCard,
   LineDivider,
 } from "../../components";
-import { COLORS, FONTS, SIZES, image, icons, dummyData } from "../../constants";
+import {
+  COLORS,
+  FONTS,
+  SIZES,
+  icons,
+  dummyData,
+  images,
+} from "../../constants";
 import { SharedElement } from "react-navigation-shared-element";
+import { AntDesign } from "@expo/vector-icons";
+import { FlatList } from "react-native-gesture-handler";
+import { MaterialIcons } from "@expo/vector-icons";
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+const HEADER_HEIGHT = 250;
 
 const CourseListing = ({ navigation, route }) => {
   const { category, sharedElementPrefix } = route.params;
 
+  const flatListRef = React.useRef();
+  const scrollY = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const headerSharedValue = useSharedValue(80);
+
   // Handler
-  function backHandler() {
+  backHandler = () => {
     navigation.goBack();
-  }
+  };
   // Render
   function renderHeader() {
+    const inputRange = [0, HEADER_HEIGHT - 50];
+    headerSharedValue.value = withDelay(
+      500,
+      withTiming(0, {
+        duration: 500,
+      })
+    );
+
+    const headerFadeAnimatedStyle = useAnimatedStyle(() => {
+      return {
+        opacity: interpolate(headerSharedValue.value, [80, 0], [0, 1]),
+      };
+    });
+
+    const headerTranslateAnimatedStyle = useAnimatedStyle(() => {
+      return {
+        transform: [
+          {
+            translateY: headerSharedValue.value,
+          },
+        ],
+      };
+    });
+
+    const headerHeightAnimatedStyle = useAnimatedStyle(() => {
+      return {
+        height: interpolate(
+          scrollY.value,
+          inputRange,
+          [HEADER_HEIGHT, 120],
+          Extrapolate.CLAMP
+        ),
+      };
+    });
+
+    const headerHideOnScrollAnimatedStyle = useAnimatedStyle(() => {
+      return {
+        opacity: interpolate(scrollY.value, [80, 0], [0, 1], Extrapolate.CLAMP),
+        transform: [
+          {
+            translateY: interpolate(
+              scrollY.value,
+              inputRange,
+              [0, 200],
+              Extrapolate.CLAMP
+            ),
+          },
+        ],
+      };
+    });
     return (
       <Animated.View
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          height: 250,
-          overflow: "hidden",
-        }}
+        style={[
+          {
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 250,
+            overflow: "hidden",
+          },
+          headerHeightAnimatedStyle,
+        ]}
       >
         {/* Bg image */}
         <SharedElement
@@ -55,11 +138,14 @@ const CourseListing = ({ navigation, route }) => {
         </SharedElement>
         {/* Title */}
         <Animated.View
-          style={{
-            position: "absolute",
-            bottom: 70,
-            left: 30,
-          }}
+          style={[
+            {
+              position: "absolute",
+              bottom: 70,
+              left: 30,
+            },
+            headerHideOnScrollAnimatedStyle,
+          ]}
         >
           <SharedElement
             id={`${sharedElementPrefix}-CategoryCard-Bg-${category?.id}`}
@@ -77,14 +163,9 @@ const CourseListing = ({ navigation, route }) => {
           </SharedElement>
         </Animated.View>
         {/* Back Button */}
-        <Animated.View>
-          <IconButton
-            icon={icons.back}
-            iconStyle={{
-              tintColor: COLORS.black,
-              width: 30,
-            }}
-            containerStyle={{
+        <Animated.View style={headerFadeAnimatedStyle}>
+          <View
+            style={{
               position: "absolute",
               top: 40,
               left: 20,
@@ -93,14 +174,100 @@ const CourseListing = ({ navigation, route }) => {
               alignItems: "center",
               justifyContent: "center",
               borderRadius: 25,
-              backgroundColor: COLORS.white,
+              // backgroundColor: COLORS.white,
             }}
-            onPress={() => {
-              backHandler();
+          >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Text>
+                <AntDesign name="leftcircle" size={45} color="white" />
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Animated.View>
+        {/* Category Image */}
+        <Animated.Image
+          source={images.mobile_image}
+          reseizeMode="contain"
+          style={[
+            {
+              position: "absolute",
+              right: 40,
+              bottom: -40,
+              width: 100,
+              height: 200,
+            },
+            headerFadeAnimatedStyle,
+            headerTranslateAnimatedStyle,
+            headerHideOnScrollAnimatedStyle,
+          ]}
+        />
+      </Animated.View>
+    );
+  }
+
+  function renderResults() {
+    return (
+      <AnimatedFlatList
+        ref={flatListRef}
+        data={dummyData.courses_list_2}
+        keyExtractor={(item) => `Results-${item.id}`}
+        contentContainerStyle={{
+          paddingHorizontal: SIZES.padding,
+        }}
+        showsHorizontalScrollIndicator={false}
+        scrollEventThrottle={16}
+        keyboardDismissMode="on-drag"
+        onScroll={onScroll}
+        ListHeaderComponent={
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 270,
+              marginBottom: SIZES.base,
+            }}
+          >
+            {/* Results */}
+            <Text
+              style={{
+                flex: 1,
+                ...FONTS.body3,
+              }}
+            >
+              2,789 Results
+            </Text>
+            {/* Filter */}
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 10,
+                backgroundColor: COLORS.primary,
+              }}
+            >
+              <MaterialIcons name="filter-list" size={24} color="white" />
+            </View>
+          </View>
+        }
+        renderItem={({ item, index }) => (
+          <HorizontalCourseCard
+            course={item}
+            containerStyle={{
+              marginVertical: SIZES.padding,
+              marginTop: index == 0 ? SIZES.radius : SIZES.padding,
             }}
           />
-        </Animated.View>
-      </Animated.View>
+        )}
+        ItemSeparatorComponent={() => (
+          <LineDivider
+            lineStyle={{
+              backgroundColor: COLORS.gray20,
+            }}
+          />
+        )}
+      />
     );
   }
   return (
@@ -110,6 +277,9 @@ const CourseListing = ({ navigation, route }) => {
         backgroundColor: COLORS.white,
       }}
     >
+      {/* Result */}
+
+      {renderResults()}
       {/* Header */}
       {renderHeader()}
     </View>
